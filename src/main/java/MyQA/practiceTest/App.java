a04dbf478d5a5b4f93c5443cc7d0b6e5fa15a8f6
SuprajaMure <38382178+SuprajaMure@users.noreply.github.com>
2018-04-14 15:25:35 -0500

    Update App.java

--- src/main/java/MyQA/practiceTest/App.java
+++ src/main/java/MyQA/practiceTest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "updated Hello World!" );
+        System.out.println( "second updated Hello World!" );
     }
 }
